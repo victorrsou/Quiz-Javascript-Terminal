4c0770c1b1de7cf11fecf28f7ffea4bd9122cf60
Victor <133800168+victorrsou@users.noreply.github.com>
2023-06-28 11:45:03 -0300

    Adição de comentários

--- quiz.js
+++ quiz.js
@@ -32,8 +32,11 @@ var quiz10 = new quiz("As obras Abaporu, Operários e Antropofagia foram pintada
 perguntas_cadastradas.push(quiz1, quiz2, quiz3, quiz4, quiz5, quiz6, quiz7, quiz8, quiz9, quiz10)
 contador_perguntas = perguntas_cadastradas.length
 
+// Estrutura principal, que "varre" todas as perguntas
 for (cont = 0; (cont < contador_perguntas); cont++) {
     console.log(`${perguntas_cadastradas[cont].pergunta} \n a) ${perguntas_cadastradas[cont].alternativa[0]} \n b) ${perguntas_cadastradas[cont].alternativa[1]} \n c) ${perguntas_cadastradas[cont].alternativa[2]} \n d) ${perguntas_cadastradas[cont].alternativa[3]} \n e) ${perguntas_cadastradas[cont].alternativa[4]}`)
+
+    // filtro para o que vai ser inserido dentro de resposta digitada.
     while(true) {
         resposta_inserida = prompt("Digite a letra da alternativa correta: ")
         resposta_inserida = resposta_inserida.toLowerCase()
@@ -42,11 +45,14 @@ for (cont = 0; (cont < contador_perguntas); cont++) {
         }
         console.log("Opção inválida. Digite somente 'A', 'B', 'C', 'D' ou 'E'")
     }
+
+    // aqui verifica qual é o índice da alternativa correta.
     for (i = 0; i < (perguntas_cadastradas[cont].alternativa.length); i++) {
         if (perguntas_cadastradas[cont].alternativa[i] == perguntas_cadastradas[cont].resposta)
             resposta_correta = perguntas_cadastradas[cont].alternativa[i]
     }
 
+    // estrutura switch para comparação das resposta e somatória da qtd de acerto e qtd de pontos.
     switch (resposta_inserida) {
         case "a":
             if (perguntas_cadastradas[cont].alternativa[0] == resposta_correta) {
@@ -95,5 +101,7 @@ for (cont = 0; (cont < contador_perguntas); cont++) {
             break
     }
 }
+
+// mensagem final ao concluir a rodada de perguntas.
 
 console.log(`Parabéns! você acertou ${qtd_acertos} questões, totalizando ${qtd_pontos} pontos. Até a próxima.`)
